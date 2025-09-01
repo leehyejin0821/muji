c21313816f68eb6ae2c9544085aa086a5ff27c03
leehyejin0821 <vincentgpwls@gmail.com>
2025-09-01 20:53:08 +0900

    second commit

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
 document.addEventListener(`DOMContentLoaded`, function () {
-  // //scoll event
-  // window.addEventListener(`scroll`, () => {
-  //   //윈도우에 스크롤할때 (document에 스크롤해도됨)
-  //   //변수를 설정해서 스크롤 상단값 저장
-  //   const scrollTopData = window.scrollY;
-
-  //   console.log(scrollTopData);
-
-  //   const header = document.querySelectorAll(`.header-area`);
-
-  //   if (scrollTopData >= 510) {
-  //     for (let on of header) {
-  //       on.classList.add(`scroll`);
-  //     }
-  //   } else {
-  //     for (let on of header) {
-  //       on.classList.remove(`scroll`);
-  //     }
-  //   }
-  // });
+  // 스크롤이벤트를 함수하나만들어서 담아줌
+  // if조건문으로 윈도우 너비값저장
+  // 윈도우 너비값이 970이하면 스크롤 이벤트함수 실행
+  // 이상이면 제거
+  // 리사이징도 추가 - 새로고침누르지않아도 너비줄이거나 늘릴때 바로 적용됨
 
   const windowWidth = window.innerWidth;
   // 윈도우 너비 저장하는 함수
 
   const scrollMobile = () => {
-    // if (windowWidth <= 970) {
-    // } else {
-    // }
     window.addEventListener(`scroll`, () => {
       const scrollTopData = window.scrollY;
       console.log(scrollTopData);
@@ -49,10 +31,41 @@ document.addEventListener(`DOMContentLoaded`, function () {
   window.addEventListener("resize", () => {
     scrollMobile();
   });
-}); //end
 
-// 스크롤이벤트를 함수하나만들어서 담아줌
-// if조건문으로 윈도우 너비값저장
-// 윈도우 너비값이 970이하면 스크롤 이벤트함수 실행
-// 이상이면 제거
-// 리사이징도 추가 - 새로고침누르지않아도 너비줄이거나 늘릴때 바로 적용됨
+  // 사업자정보 클릭이벤트
+  const inforTitle = document.querySelector(`.bot-infor-txt1`);
+  const infor = document.querySelector(`.bot-infor-txt2`);
+
+  inforTitle.addEventListener(`click`, function () {
+    infor.classList.toggle(`on`);
+  });
+
+  //서브메뉴 호버이벤트
+  const submenuTab = document.querySelectorAll(`.menu-box li`);
+  const subMenuBox = document.querySelector(`.sub-menu-box`);
+
+  for (const li of submenuTab) {
+    li.addEventListener(`mouseenter`, function () {
+      subMenuBox.classList.add(`active`);
+
+      // 탭메뉴 연결
+      const tab = this.getAttribute(`data-tab`);
+      // 이 아이는 html의 li 안에있는 데이터를 불러와준거임
+      const subMenu = document.querySelectorAll(`.sub-menu`);
+
+      // 전체적으로 서브메뉴 먼저제거
+      for (const tabContent of subMenu) {
+        tabContent.classList.remove(`active`);
+      }
+
+      // data-tab 에 작성된 데이터명과 동일한 아이디명을 가진 서브메뉴는 출력
+      const changeTab = document.querySelector(`#${tab}`);
+      changeTab.classList.add(`active`);
+    });
+  }
+
+  // 서브메뉴박스에서 마우스 나가면 기존 상태로 변경
+  subMenuBox.addEventListener(`mouseleave`, function () {
+    this.classList.remove(`active`);
+  });
+}); //end
